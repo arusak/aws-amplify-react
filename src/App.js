2315eb10b40f5b0c35885ab5d5e602ac703b201c
Anton Rusak <4231915+arusak@users.noreply.github.com>
2021-09-14 15:11:08 +0300

    Using backend in web app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import './App.css';
+import {API} from 'aws-amplify';
+import {useState, useEffect} from 'react';
+import {format} from 'date-fns';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        HAPPY HACKING
-      </header>
-    </div>
-  );
+    const [backendData, setBackendData] = useState('');
+    const [selectedDate, setSelectedDate] = useState(new Date());
+
+    useEffect(() => {
+        query(selectedDate);
+    }, [selectedDate]);
+
+    const query = async (date) => {
+        const queryDate = format(date, 'yyyy-MM-dd');
+        const formattedDate = await API.get('VdhrParserApi', `/${queryDate}`, {});
+        setBackendData(formattedDate);
+    };
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <input style={{fontSize: '200%'}} type="date" onChange={evt => setSelectedDate(evt.target.valueAsDate)}/>
+                <p>Selected Date: <b>{selectedDate.toString()}</b></p>
+                {backendData && <p>Processed by backend: {backendData}</p>}
+            </header>
+        </div>
+    );
 }
 
 export default App;
